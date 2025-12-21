93703913d86f01d57d128873acd82c7ef527d303
LavBujak <s4094261@student.rmit.edu.au>
2025-12-21 17:51:23 +1100

    changes

--- frontend/app/api/users/route.ts
+++ frontend/app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { pool } from "@/lib/db";
+import pool from "@backend/lib/db";
 
 export async function GET() {
   const result = await pool.query("SELECT * FROM users;");
---
+++ frontend/next.config.ts
@@ -0,0 +1,11 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({ configFile: "./tsconfig.json" }),
+    ];
+    return config;
+  },
+};
